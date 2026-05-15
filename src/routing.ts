80276815b41b949b172944933e00d1b71bf97f0e
jawhar sghaier <jsghaier@viamedis.fr>
2026-05-15 13:16:50 +0200

    Update routing.ts

--- src/routing.ts
+++ src/routing.ts
@@ -4,6 +4,7 @@ export const routing = defineRouting({
   locales: ['fr', 'en', 'ar'],
   defaultLocale: 'fr',
   localePrefix: 'as-needed',
+  localeDetection: false,
 })
 
 export type Locale = (typeof routing.locales)[number]
